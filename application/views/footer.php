126ed972330fae1f59f7511beb0148a82ca62f7d
Beldion <bgbalanan@gmail.com>
2016-02-01 13:38:38 +0700

    New Files
    Lawrence

---
+++ application/views/footer.php
@@ -0,0 +1,26 @@
+ <footer id="footer" class="midnight-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Faq</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
+
+    <script src="<?php echo base_url('assets/js/jquery.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/jquery.prettyPhoto.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/jquery.isotope.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/wow.min.js'); ?>"></script>
+</body>
+</html>
